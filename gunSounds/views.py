1b484f6b28501af64d5ce974189a7af9cbb2da3e
Zeddicus <jordan.bailey2@yahoo.com>
2025-07-22 09:46:33 -0700

    Added new view for gun models and .gitignores

--- gunSounds/views.py
+++ gunSounds/views.py
@@ -1,10 +1,23 @@
 from django.shortcuts import render
-from django.template import loader
 
 def sound_list(request):
+    # Prepare all 10 increments for Frontier 73C
     sounds = [
-        {"title": "Picard Song", "path": "gunSounds/audio/picard.mp3"},
-        
+        {
+            "title": "Frontier 73C",
+            "img": "gunSounds/images/Frontier_73C.jpg",
+            "audio_clips": [
+                {"label": "100m", "path": "gunSounds/audio/Frontier73C_100.wav"},
+                {"label": "200m", "path": "gunSounds/audio/Frontier73C_200.wav"},
+                {"label": "300m", "path": "gunSounds/audio/Frontier73C_300.wav"},
+                {"label": "400m", "path": "gunSounds/audio/Frontier73C_400.wav"},
+                {"label": "500m", "path": "gunSounds/audio/Frontier73C_500.wav"},
+                {"label": "600m", "path": "gunSounds/audio/Frontier73C_600.wav"},
+                {"label": "700m", "path": "gunSounds/audio/Frontier73C_700.wav"},
+                {"label": "800m", "path": "gunSounds/audio/Frontier73C_800.wav"},
+                {"label": "900m", "path": "gunSounds/audio/Frontier73C_900.wav"},
+                {"label": "1000m", "path": "gunSounds/audio/Frontier73C_1000.wav"},
+            ],
+        }
     ]
     return render(request, "gunSounds/list.html", {"sounds": sounds})
-
